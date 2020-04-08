6fcdf045183f2b2c17c1e1192a24ee35a7f5bf28
Aldian Fazrihady <mobile@aldian.net>
2020-04-08 20:30:31 +0700

    p1s3t12 create needer request

--- boots_main.py
+++ boots_main.py
@@ -1117,5 +1117,71 @@ class AddHashtag(CommonPostHandler):
         return {'success': RC.success, 'return_msg': return_msg, 'debug_data': debug_data}
 
 
+@app.route(Services.web_request.create_needer_request.url, methods=["OPTIONS", "POST"])
+@wrap_webapp_class(Services.web_request.create_needer_request.name)
+class CreateNeederRequest(CommonPostHandler):
+    def process_request(self):
+        task_id = 'web-requests:CreateNeederRequest:process_request'
+        debug_data = []
+        return_msg = task_id + ": "
+        transaction_user_uid = "1"
+
+        # input validation
+        user_uid = unicode(self.request.get(TaskArguments.s3t12_user_uid, ""))
+
+        call_result = self.ruleCheck([
+            [user_uid, PostDataRules.internal_uid],
+        ])
+
+        debug_data.append(call_result)
+        if call_result['success'] != RC.success:
+            return_msg += "input validation failed"
+            return {'success': RC.input_validation_failed, 'return_msg': return_msg, 'debug_data': debug_data}
+
+        user_uid = long(user_uid)
+
+        user_key = ndb.Key(DsP1.users._get_kind(), user_uid)
+        call_result = DSF.kget(user_key)
+        if call_result['success'] != RC.success:
+            return_msg += "Failed to load user from datastore"
+            return {
+                'success': RC.datastore_failure, 'return_msg': return_msg, 'debug_data': debug_data,
+            }
+        user = call_result['get_result']
+        if not user:
+            return_msg += "User doesn't exist"
+            return {
+                'success': RC.input_validation_failed, 'return_msg': return_msg, 'debug_data': debug_data,
+            }
+        # </end> input validation
+
+        # create transaction to create needer
+        pma = {
+            TaskArguments.s1t3_user_uid: user_uid,
+        }
+
+        task_sequence = [{
+            'name': TaskNames.s1t3,
+            'PMA': pma,
+        }]
+
+        try:
+            task_sequence = unicode(json.JSONEncoder().encode(task_sequence))
+        except Exception as e:
+            return_msg += "JSON encoding of task_queue failed with exception:%s" % e
+            return {'success': False, 'return_msg': return_msg, 'debug_data': debug_data}
+
+        task_functions = CTF()
+        call_result = task_functions.createTransaction(GSB.project_id, transaction_user_uid, task_id,
+                                                       task_sequence)
+        debug_data.append(call_result)
+        if call_result['success'] != RC.success:
+            return_msg += 'failed to add task queue function'
+            return {'success': call_result['success'], 'debug_data': debug_data, 'return_msg': return_msg}
+        #</end> create transaction to create needer
+
+        return {'success': RC.success, 'return_msg': return_msg, 'debug_data': debug_data}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
